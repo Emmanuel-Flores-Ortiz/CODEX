ac2e4da8d8d92ec8613b5aeebcbe0adda3f22e59
Eduardo Flores <eduardo.flores@uniat.edu.mx>
2026-06-01 11:32:03 -0700

    guildex.h creada y struct Quest agregado

---
+++ guildex.cpp
@@ -0,0 +1,9 @@
+#include "guildex.h"
+
+struct Quest {
+	std::string lugar;
+	std::string personajeInvolucrado;
+	int numeroDePersonajes;
+	std::string accion;
+	int recompensa;
+};
---
+++ guildex.h
@@ -0,0 +1,6 @@
+#ifndef GUILDEX_H
+#define GUILDEX_H
+
+struct Quest;
+
+#endif
